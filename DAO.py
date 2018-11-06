268da56aec09fda02537d069f7ca8b9e8c01042f
Wilder Lizama Garcia <wilderlizama@gmail.com>
2018-11-06 14:39:27 -0500

    primeras pruebas POO

---
+++ DAO.py
@@ -0,0 +1,56 @@
+class Empresa():
+    
+    def __init__(self, razon_social, ruc, clave_sol):
+        self._razon_social = razon_social
+        self._ruc = ruc
+        self._clave_sol = clave_sol
+
+    @property
+    def razon_social(self):
+        return self._razon_social
+    
+    @razon_social.setter
+    def razon_social(self, value):
+        self._razon_social = value
+
+    @property
+    def ruc(self):
+        return self._ruc
+    
+    @ruc.setter
+    def ruc(self, value):
+        self._ruc = value
+
+    @property
+    def clave_sol(self):
+        return self._clave_sol
+    
+    @clave_sol.setter
+    def clave_sol(self, value):
+        self._clave_sol = value
+
+    def __str__(self):
+        return "{0} {1}".format(self.ruc, self.razon_social)
+
+
+class ClaveSol():
+
+    def __init__(self, usuario, contrasenha):
+        self._usuario = usuario
+        self._contrasenha = contrasenha
+
+    @property
+    def usuario(self):
+        return self._usuario
+    
+    @usuario.setter
+    def usuario(self, value):
+        self._usuario = value
+
+    @property
+    def contrasenha(self):
+        return self._contrasenha
+    
+    @contrasenha.setter
+    def contrasenha(self, value):
+        self._contrasenha = value
--- __main__.py
+++ __main__.py
@@ -1,7 +1,49 @@
 import json
+from DAO import Empresa, ClaveSol
+
+def getListaEmpresas():
+    lista_empresas = []
+
+    data = json.loads(open("empresas.json").read())
+    for empresa in data["empresas"]:
+        emp = Empresa(
+            empresa["razon_social"],
+            empresa["ruc"],
+            ClaveSol(
+                empresa["clave_sol"]["usuario"],
+                empresa["clave_sol"]["contrasenha"]
+            )
+        )
+
+        lista_empresas.append(emp)
+    
+    return lista_empresas
+
+
+def getEmpresaSeleccionada():
+    line_separador = "-" * 50
+
+    print("""\
+    \nSELECCIONE LA EMPRESA\
+    \n{0}\
+    \n # | Empresa\
+    \n{0}\
+    """.format(line_separador))
+
+    empresas = getListaEmpresas()
+    for empresa in empresas:
+        print(" {} | {}".format(empresas.index(empresa) + 1, empresa))
+
+    indx_empresa_seleccionada = int(input("Ingrese el número: ")) - 1
+
+    return empresas[indx_empresa_seleccionada]
+
 
 def main():
-    pass
+    empresa_seleccionada = getEmpresaSeleccionada()
+
+    print(empresa_seleccionada.clave_sol.usuario)
+    print(empresa_seleccionada.clave_sol.contrasenha)
 
 if __name__ == '__main__':
     main()
