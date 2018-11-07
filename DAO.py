f1ef8affbcc82eb705e0cd4541454ceddad623c5
Wilder Lizama Garcia <wilderlizama@gmail.com>
2018-11-07 13:11:17 -0500

    Prueba captura de datos

--- DAO.py
+++ DAO.py
@@ -54,3 +54,28 @@ class ClaveSol():
     @contrasenha.setter
     def contrasenha(self, value):
         self._contrasenha = value
+
+
+class TipoDocumento():
+    def __init__(self, codigo, descripcion):
+        self._codigo = codigo
+        self._descripcion = descripcion
+
+    @property
+    def codigo(self):
+        return self._codigo
+    
+    @codigo.setter
+    def codigo(self, value):
+        self._codigo = value
+
+    @property
+    def descripcion(self):
+        return self._descripcion
+    
+    @descripcion.setter
+    def descripcion(self, value):
+        self._descripcion = value
+
+    def __str__(self):
+        return "{0} {1}".format(self.codigo, self.descripcion)
--- __main__.py
+++ __main__.py
@@ -1,10 +1,17 @@
 import json
-from DAO import Empresa, ClaveSol
+from DAO import Empresa, ClaveSol, TipoDocumento
+import os
+
+LINE_SEPARADOR = "-"
+
+def lineSeparator(len):
+    return LINE_SEPARADOR * len
+
 
 def getListaEmpresas():
     lista_empresas = []
 
-    data = json.loads(open("empresas.json").read())
+    data = json.loads(open("config/empresas.json").read())
     for empresa in data["empresas"]:
         emp = Empresa(
             empresa["razon_social"],
@@ -20,30 +27,95 @@ def getListaEmpresas():
     return lista_empresas
 
 
-def getEmpresaSeleccionada():
-    line_separador = "-" * 50
+def getListaTiposDocs():
+    lista_tipos_docs = []
 
+    data = json.loads(open("config/tipos_docs.json").read())
+    for tipo_doc in data["tipos_docs"]:
+        tdoc = TipoDocumento(
+            tipo_doc["codigo"],
+            tipo_doc["descripcion"]
+        )
+
+        lista_tipos_docs.append(tdoc)
+    
+    return lista_tipos_docs
+
+
+def getEmpresaSeleccionada():
     print("""\
-    \nSELECCIONE LA EMPRESA\
+    \nLISTA DE EMPRESAS\
     \n{0}\
-    \n # | Empresa\
+    \n # | RUC         | Empresa\
     \n{0}\
-    """.format(line_separador))
+    """.format(lineSeparator(50)))
 
     empresas = getListaEmpresas()
     for empresa in empresas:
-        print(" {} | {}".format(empresas.index(empresa) + 1, empresa))
+        print(" {} | {} | {}".format(
+            empresas.index(empresa) + 1,
+            empresa.ruc,
+            empresa.razon_social))
 
-    indx_empresa_seleccionada = int(input("Ingrese el número: ")) - 1
+    indx_empresa_seleccionada = int(input("Empresa: ")) - 1
 
     return empresas[indx_empresa_seleccionada]
 
 
+def getTipoDocSeleccionado():
+    print("""\
+    \nLISTA TIPOS DE DOCUMENTOS\
+    \n{0}\
+    \n # |  Cod  | Descripción\
+    \n{0}\
+    """.format(lineSeparator(80)))
+
+    tipos_docs = getListaTiposDocs()
+    for tipo_doc in tipos_docs:
+        print(" {} |  {}   | {}".format(
+            tipos_docs.index(tipo_doc) + 1,
+            tipo_doc.codigo,
+            tipo_doc.descripcion))
+
+    indx_tipos_doc_seleccionado = int(input("Tipo documento: ")) - 1
+
+    return tipos_docs[indx_tipos_doc_seleccionado]
+
+
 def main():
     empresa_seleccionada = getEmpresaSeleccionada()
+    
+    os.system('cls')  # windows OS
+    
+    print("Empresa: {0}".format(empresa_seleccionada))
 
-    print(empresa_seleccionada.clave_sol.usuario)
-    print(empresa_seleccionada.clave_sol.contrasenha)
+    tipo_doc_seleccionado = getTipoDocSeleccionado()
+
+    os.system('cls')  # windows OS
+
+    print("Empresa: {0}".format(empresa_seleccionada))
+    print("Tipo Documento: {0}".format(tipo_doc_seleccionado))
+
+    serie = input("Serie: ")
+
+    os.system('cls')  # windows OS
+
+    print("Empresa: {0}".format(empresa_seleccionada))
+    print("Tipo Documento: {0}".format(tipo_doc_seleccionado))
+    print("Serie: {0}".format(serie))
+
+    numero = int(input("Número: "))
+
+    os.system('cls')  # windows OS
+
+    print("Empresa: {0}".format(empresa_seleccionada))
+    print("Tipo Documento: {0}".format(tipo_doc_seleccionado))
+    print("Serie: {0}".format(serie))
+    print("Número: {0}".format(numero))
+
+
+    # print(empresa_seleccionada.clave_sol.usuario)
+    # print(empresa_seleccionada.clave_sol.contrasenha)
 
 if __name__ == '__main__':
     main()
