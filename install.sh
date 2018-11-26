e14714d3bc51744558bb93ee537ad504a2dab65e
Wilder Lizama Garcia <wilderlizama@gmail.com>
2018-11-26 12:29:37 -0500

    Script para crear ejecutable con pyinstaller

--- install.sh
+++ install.sh
@@ -1,1 +1,38 @@
-pyinstaller --clean -n sbsSOAP --add-data "./config/empresas.sample.json;config" --add-data "./config/paths.json;config" --add-data "./config/tipos_docs.json;config" __main__.py
+#!/bin/sh
+
+## pyinstaller how to use
+# https://pyinstaller.readthedocs.io/en/stable/usage.html
+
+
+## variables globales
+dist_folder="dist"
+config_folder="config"
+cdr_folder="CDRFiles"
+
+
+# creador de ejecutable
+pyinstaller --distpath $dist_folder -F --clean -n sbsSOAP __main__.py
+
+
+# crear carpetas necesarias para ejecutable
+mkdir $dist_folder/$config_folder
+
+mkdir $dist_folder/$cdr_folder
+
+
+## copiar archivos de configuración
+cp config/* ./$dist_folder/$config_folder
+
+
+## copiar archivos varios
+cp ./LICENSE ./$dist_folder/
+
+cp ./README.md ./$dist_folder/
+
+
+echo ""
+echo "##################################################"
+echo "Finalzado, presiona cualquier tecla para cerrar"
+echo "##################################################"
+
+read myothervar
